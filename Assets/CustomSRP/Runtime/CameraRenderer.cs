dd7e0eb9846774b9dad18f1fe57c5e66feebea34
Andrey Astapov <a.astapov@carx-tech.com>
2023-11-10 01:21:22 +0300

    Clear color settings
    this commit finishes the 1st tutor in catlike coding

--- Assets/CustomSRP/Runtime/CameraRenderer.cs
+++ Assets/CustomSRP/Runtime/CameraRenderer.cs
@@ -35,7 +35,12 @@ public partial class CameraRenderer {
 
 	void Setup () {
 		context.SetupCameraProperties(camera);
-		buffer.ClearRenderTarget(true, true, Color.clear);
+		CameraClearFlags flags = camera.clearFlags;
+		buffer.ClearRenderTarget(
+			flags <= CameraClearFlags.Depth,
+			flags == CameraClearFlags.Color,
+			flags == CameraClearFlags.Color ?
+				camera.backgroundColor.linear : Color.clear);
 		buffer.BeginSample(bufferName);
 		ExecuteBuffer();
 	}
