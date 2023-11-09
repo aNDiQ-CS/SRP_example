224ab452203d18101a28ccb0b3c78f2b92e5391a
Andrey Astapov <a.astapov@carx-tech.com>
2023-11-10 00:56:59 +0300

    SRP basics

---
+++ Assets/CustomSRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer {
+
+	ScriptableRenderContext context;
+
+	Camera camera;
+	CullingResults cullingResults;
+
+	const string bufferName = "RenderCamera";
+	CommandBuffer buffer = new CommandBuffer {
+		name = bufferName
+	};
+
+	static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+
+	public void Render(ScriptableRenderContext context, Camera camera)
+	{
+		this.context = context;
+		this.camera = camera;
+
+		if (!Cull()) {
+			return;
+		}
+
+		Setup();
+		DrawVisibleGeometry();
+		DrawUnsupportedShaders();
+		DrawGizmos();
+		Submit();
+	}
+
+	void Setup () {
+		context.SetupCameraProperties(camera);
+		buffer.ClearRenderTarget(true, true, Color.clear);
+		buffer.BeginSample(bufferName);
+		ExecuteBuffer();
+	}
+
+	bool Cull ()
+	{
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)) {
+			cullingResults = context.Cull(ref p);
+			return true;
+		}
+		return false;
+	}
+
+	void DrawVisibleGeometry ()
+	{
+		var sortingSettings = new SortingSettings(camera)
+		{
+			criteria = SortingCriteria.CommonOpaque
+		};
+		var drawingSettings = new DrawingSettings(
+			unlitShaderTagId, sortingSettings
+		);
+		var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+		context.DrawRenderers(
+			cullingResults, ref drawingSettings, ref filteringSettings
+		);
+
+		context.DrawSkybox(camera);
+
+		//Draw transparent geometry
+		sortingSettings.criteria = SortingCriteria.CommonTransparent;
+		drawingSettings.sortingSettings = sortingSettings;
+		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+		context.DrawRenderers(
+			cullingResults, ref drawingSettings, ref filteringSettings
+		);
+	}
+
+	void Submit () {
+		buffer.EndSample(bufferName);
+		ExecuteBuffer();
+		context.Submit();
+	}
+
+	void ExecuteBuffer () {
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+}
---
+++ Assets/CustomSRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+	CameraRenderer renderer = new CameraRenderer();
+
+	protected override void Render(
+		ScriptableRenderContext context, Camera[] cameras
+	)
+	{
+		for (int i = 0; i < cameras.Length; i++) {
+			renderer.Render(context, cameras[i]);
+		}
+	}
+}
